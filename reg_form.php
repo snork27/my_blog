b5b701d5a00f5142a8cdac763a3f78d39ee4f0c8
snork27 <92690025+snork27@users.noreply.github.com>
2021-11-24 01:17:29 +0300

    Add files via upload

--- reg_form.php
+++ reg_form.php
@@ -8,22 +8,48 @@ $password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
 if (mb_strlen($login) < 5 || mb_strlen($login) > 15) {
     echo "Недопустимая длина логина";
     exit ();
-} 
+}
 else if (mb_strlen($username) < 5 || mb_strlen($username) > 15) {
     echo "Недопустимая длина юзернейма";
     exit ();
-} 
+}
 else if (mb_strlen($password) < 5 || mb_strlen($password) > 15) {
     echo "Недопустимая длина пароля";
     exit ();
-} 
+}
 
 
-$mysql = new mysqli_connect ('localhost', 'root', 'root', 'register-bd');
-$mysql -> query("INSERT INTO `users` (`username`, `login`, `email`, `password`) VALUES ('$username', '$login', '$email', '$password')");
+$db_host = '127.0.0.1';
+$db_user = 'root';
+$db_password = 'root';
+$db_db = 'register-bd';
+$db_port = 8889;
 
-$mysql -> close();
+$mysqli = new mysqli(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db,
+    $db_port
+);
 
-header('Location: ../');
+if ($mysqli->connect_error) {
+    echo 'Errno: '.$mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: '.$mysqli->connect_error;
+    exit();
+}
+
+echo 'Success: A proper connection to MySQL was made.';
+echo '<br>';
+echo 'Host information: '.$mysqli->host_info;
+echo '<br>';
+echo 'Protocol version: '.$mysqli->protocol_version;
+
+$mysqli->query("INSERT INTO `users` (`username`, `login`, `email`, `password`) VALUES ('$username', '$login', '$email', '$password')");
+
+$mysqli->close();
+
+header('Location: /reg_form.html');
 
 ?>
